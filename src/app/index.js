a1d0d9ee390030278f3380dc733341edb4d381ff
Patrick Logan <jpatricklogan@gmail.com>
2017-08-01 07:18:46 -0600

    props with state example

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,25 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['wash up', 'eat some cheese', 'take a nap']
+    }
+  },
   render: function(){
     return(
-      <h1>Helloooooo, Ninjas</h1>
+      <div id="todo-list">
+        <p>The busiest people have the most leisure...</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
+      </div>
     );
   }
 });
+
+var myCheese={name: 'Camenbert', smellFactor: 'Extreme pong', price: '$3.50'};
 
 ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
